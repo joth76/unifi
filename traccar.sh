7d1deb772146be1c0794b8391e926bc820965944
joth76 <joth@chromium.org>
2019-10-28 11:55:52 +0000

    add back in fail2ban

--- traccar.sh
+++ traccar.sh
@@ -101,10 +101,18 @@ if [ "x${haveged}" != "xinstall ok installed" ]; then
 		echo "Haveged installed"
 	fi
 fi
+
 certbot=$(dpkg-query -W --showformat='${Status}\n' certbot 2>/dev/null)
 if [ "x${certbot}" != "xinstall ok installed" ]; then
 if (apt-get -qq install -y -t ${release}-backports certbot >/dev/null) || (apt-get -qq install -y certbot >/dev/null); then
 		echo "CertBot installed"
+	fi
+fi
+
+f2b=$(dpkg-query -W --showformat='${Status}\n' fail2ban 2>/dev/null)
+if [ "x${f2b}" != "xinstall ok installed" ]; then 
+	if apt-get -qq install -y fail2ban >/dev/null; then
+			echo "Fail2Ban installed"
 	fi
 fi
 
